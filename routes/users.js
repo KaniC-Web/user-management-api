fbc39c95ebd7b532917996ee31908a531a429c76
Kani Chandrasekar <kanimozhichandrasekhar05@gmail.com>
2025-07-28 10:50:18 +0100

    getting all the users

--- routes/users.js
+++ routes/users.js
@@ -2,3 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
+
+// GET all users
+router.get('/', (req, res) => {
+  db.query('SELECT * FROM users', (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(results);
+  });
+});
