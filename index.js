646b7331e8a43c6076c9617480de83f25d5e9833
Kani Chandrasekar <kanimozhichandrasekhar05@gmail.com>
2025-07-28 11:25:07 +0100

    fixed the path issue in index js file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // index.js
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const userRoutes = require('./routes/users');
 
 // Middleware
 app.use(express.json());
@@ -9,16 +9,13 @@ app.use(express.json());
 // Routes
 app.use('/api/users', userRoutes);
 
+// Root Ping
 app.get('/', (req, res) => {
   res.send('User Management API is working!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
 
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
